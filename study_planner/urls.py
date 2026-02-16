aff17ac23426206067ab0f17e4819e336190c4ed
Yiğit Emir Cengiz <yigitemircengiz@gmail.com>
2026-02-17 00:33:11 +0300

    Fix import and update dashboard path in urls.py

--- study_planner/urls.py
+++ study_planner/urls.py
@@ -4,9 +4,10 @@ from core.views import dashboard, toggle_task, register, delete_task
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', register, name='register'),
     path('toggle-task/<int:task_id>/', toggle_task, name='toggle_task'),
     path('delete-task/<int:task_id>/', delete_task, name='delete_task'), # views. silindi
     path('accounts/', include('django.contrib.auth.urls')),
+
 ]
